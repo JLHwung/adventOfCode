6e4902b4f90bf3ce32685621d9c99e35e02ae5d9
Huáng Jùnliàng <jlhwung@gmail.com>
2019-12-02 21:39:37 -0500

    d2p2

--- 2019/day2/src/main.rs
+++ 2019/day2/src/main.rs
@@ -7,16 +7,30 @@ fn main() -> io::Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let mut values: vec::Vec<u32> = contents.split(',').map(|x| {
+    let values: vec::Vec<u32> = contents.split(',').map(|x| {
         x.trim_end().parse().unwrap()
     }).collect();
 
+    for noun in 0..99 {
+        for verb in 0..99 {
+            if intcode_interpreter(&values, noun, verb)[0] == 19690720 {
+                println!("{}", 100 * noun + verb);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn intcode_interpreter(vec: &Vec<u32>, noun: u32, verb: u32) -> Vec<u32> {
+    let mut values: Vec<u32> = vec.clone();
     /*
         non-sense
         Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
     */
-    values[1] = 12;
-    values[2] = 2;
+    values[1] = noun;
+    values[2] = verb;
 
     let mut cursor: usize = 0;
     while values[cursor] != 99 {
@@ -38,7 +52,5 @@ fn main() -> io::Result<()> {
         }
     }
 
-    println!("{}", values[0]);
-
-    Ok(())
+    values
 }
