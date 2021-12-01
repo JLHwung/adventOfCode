961f32c070f288a371f6b6ecd282541628d13016
Huáng Jùnliàng <jlhwung@gmail.com>
2021-12-01 17:56:50 -0500

    day3

---
+++ 2020/src/day3.rs
@@ -0,0 +1,54 @@
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let raw = fs::read_to_string(fs::canonicalize("./data/day3.txt")?)?;
+    let input = process(&raw);
+    println!("Answer of p1: {}", p1(&input));
+    println!("Answer of p2: {}", p2(&input));
+    Ok(())
+}
+
+fn process(raw: &str) -> Vec<Vec<u8>> {
+    let mut result: Vec<Vec<u8>> = vec![];
+    for line in raw.split("\n") {
+        if line.is_empty() {
+            continue;
+        }
+        let vector: Vec<u8> = line
+            .chars()
+            .map(|c| match c {
+                '#' => 1,
+                '.' => 0,
+                _ => unreachable!(),
+            })
+            .collect();
+        result.push(vector);
+    }
+    result
+}
+
+fn p1(input: &Vec<Vec<u8>>) -> usize {
+    traverse_trees(input, 3, 1)
+}
+
+fn traverse_trees(input: &Vec<Vec<u8>>, right: usize, down: usize) -> usize {
+    let mut result: usize = 0;
+    let width = input[0].len();
+    let mut j = right;
+    for i in (down..input.len()).step_by(down) {
+        j = j % width;
+        if input[i][j] == 1 {
+            result += 1;
+        }
+        j += right;
+    }
+    result
+}
+
+fn p2(input: &Vec<Vec<u8>>) -> usize {
+    let data: Vec<(usize, usize)> = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    data.iter()
+        .map(|(right, down)| traverse_trees(input, *right, *down))
+        .fold(1, |acc, x| acc * x)
+}
