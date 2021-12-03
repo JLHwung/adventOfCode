a93bd21239bd1320a716a303b8a858c5cabecb3a
Huáng Jùnliàng <jlhwung@gmail.com>
2021-12-03 11:54:17 -0500

    refactor: simplify data structure

--- 2021/src/day3.rs
+++ 2021/src/day3.rs
@@ -9,64 +9,48 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn process(raw: &str) -> Vec<Vec<usize>> {
+fn process(raw: &str) -> Vec<Vec<char>> {
     let mut result: Vec<_> = vec![];
     for n in raw.split("\n") {
         if n.is_empty() {
             continue;
         }
-        result.push(
-            n.chars()
-                .map(|x| match x {
-                    '0' => 0,
-                    '1' => 1,
-                    _ => unreachable!(),
-                })
-                .collect::<Vec<usize>>(),
-        )
+        result.push(n.chars().collect())
     }
     result
 }
 
-fn p1(input: &Vec<Vec<usize>>) -> usize {
+fn p1(input: &Vec<Vec<char>>) -> usize {
     let height = input.len();
     let width = input[0].len();
-    let mut mcb: Vec<usize> = vec![];
+    let mut mcb: String = "".to_string();
     for i in 0..width {
-        let sum = input.iter().map(|x| x[i]).fold(0, |sum, x| sum + x);
+        let sum = input.iter().filter(|x| x[i] == '1').count();
         if sum > height / 2 {
-            mcb.push(1)
+            mcb.push('1')
         } else {
-            mcb.push(0)
+            mcb.push('0')
         }
     }
-    let gamma_rate = usize::from_str_radix(
-        &mcb.iter()
-            .map(|x| match x {
-                0 => '0',
-                1 => '1',
-                _ => unreachable!(),
-            })
-            .collect::<String>(),
-        2,
-    )
-    .unwrap();
+    let gamma_rate = usize::from_str_radix(&mcb, 2).unwrap();
     let epsilon_rate = (1 << width) - 1 - gamma_rate;
     gamma_rate * epsilon_rate
 }
 
-fn get_nested_common_bits(input: &Vec<Vec<usize>>, mcb: bool) -> usize {
+fn get_nested_common_bits(input: &Vec<Vec<char>>, mcb: bool) -> usize {
     let width = input[0].len();
-    let mut bits: Vec<usize> = vec![];
-    let mut filtered: Vec<&Vec<usize>> = input.into_iter().collect();
+    let mut bits: String = "".to_string();
+    let mut filtered: Vec<&Vec<char>> = input.into_iter().collect();
     for i in 0..width {
-        let sum = filtered.iter().map(|x| x[i]).fold(0, |sum, x| sum + x);
+        let sum = filtered.iter().filter(|x| x[i] == '1').count();
         let filtered_len = filtered.len();
         // If 0 and 1 are equally common, keep values with a 1 in the position being considered.
-        let mut bit = if sum + sum >= filtered_len { 1 } else { 0 };
-        if mcb == false {
-            bit ^= 1
-        }
+        let selection = if mcb {
+            sum + sum >= filtered_len
+        } else {
+            sum + sum < filtered_len
+        };
+        let mut bit = if selection { '1' } else { '0' };
         // When there is only one element, the bit is both mcb/lcb
         if filtered_len == 1 {
             bit = filtered[0][i]
@@ -74,20 +58,9 @@ fn get_nested_common_bits(input: &Vec<Vec<usize>>, mcb: bool) -> usize {
         bits.push(bit);
         filtered = filtered.into_iter().filter(|x| x[i] == bit).collect();
     }
-    usize::from_str_radix(
-        &bits
-            .iter()
-            .map(|x| match x {
-                0 => '0',
-                1 => '1',
-                _ => unreachable!(),
-            })
-            .collect::<String>(),
-        2,
-    )
-    .unwrap()
+    usize::from_str_radix(&bits, 2).unwrap()
 }
-fn p2(input: &Vec<Vec<usize>>) -> usize {
+fn p2(input: &Vec<Vec<char>>) -> usize {
     let oxygen_generator_rating = get_nested_common_bits(input, true);
     let co2_scrubber_rating = get_nested_common_bits(input, false);
 
