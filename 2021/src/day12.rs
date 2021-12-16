9a5e6ac83944a2b9ca3d4ee78da7190ffdcd3439
Huáng Jùnliàng <jlhwung@gmail.com>
2021-12-15 21:12:04 -0500

    perf: compress map node storage

--- 2021/src/day12.rs
+++ 2021/src/day12.rs
@@ -13,39 +13,61 @@ fn main() {
     println!("Answer of p2: {}", p2(&input));
 }
 
-/// The input graph is modeled by a HashMap from node to their neighbors
-type Input<'a> = HashMap<&'a str, Vec<&'a str>>;
+struct Input<'a> {
+    labels: HashMap<&'a str, u8>,
+    big_cave_ids: HashSet<u8>,
+    /// The map is modeled by a HashMap from node to their neighbors
+    map: HashMap<u8, Vec<u8>>,
+}
+
+const START_CAVE_LABEL: u8 = 0;
+const END_CAVE_LABEL: u8 = 1;
 
 fn process(raw: &str) -> Input {
-    let mut result: Input = HashMap::new();
+    let mut labels = HashMap::from([("start", START_CAVE_LABEL), ("end", END_CAVE_LABEL)]);
+    let mut map = HashMap::new();
+    let mut big_cave_ids = HashSet::new();
+    let mut register_index = |a| {
+        let index = labels.len() as u8;
+        *labels.entry(a).or_insert_with(|| {
+            if a.as_bytes()[0] <= b'Z' {
+                big_cave_ids.insert(index);
+            }
+            index
+        })
+    };
+    let mut register_edge =
+        |from_id, to_id| map.entry(from_id).or_insert_with(Vec::new).push(to_id);
+
     for line in raw.split('\n') {
         if line.is_empty() {
             continue;
         }
-        let mut iter = line.split('-');
-        let from = iter.next().unwrap();
-        let to = iter.next().unwrap();
-        let from_neighbors = result.entry(from).or_insert_with(Vec::new);
-        from_neighbors.push(to);
-        let to_neighbors = result.entry(to).or_insert_with(Vec::new);
-        to_neighbors.push(from);
+        let (from, to) = line.split_once('-').unwrap();
+
+        let (from_id, to_id) = (register_index(from), register_index(to));
+        register_edge(from_id, to_id);
+        register_edge(to_id, from_id);
     }
-    result
+    Input {
+        labels,
+        big_cave_ids,
+        map,
+    }
 }
 
-fn is_big_cave(name: &str) -> bool {
-    !name.chars().next().unwrap().is_ascii_lowercase()
-}
-
-struct P1State<'a> {
-    current_cave: &'a str,
-    visited_small_caves: HashSet<&'a str>,
+struct P1State {
+    current_cave: u8,
+    visited_small_caves: HashSet<u8>,
 }
 
 fn p1(input: &Input) -> usize {
+    let Input {
+        map, big_cave_ids, ..
+    } = input;
     let initial_state = P1State {
-        current_cave: "start",
-        visited_small_caves: ["start"].into_iter().collect::<HashSet<_>>(),
+        current_cave: START_CAVE_LABEL,
+        visited_small_caves: HashSet::from([START_CAVE_LABEL]),
     };
     let mut path_stack = vec![initial_state];
     let mut count = 0;
@@ -54,16 +76,16 @@ fn p1(input: &Input) -> usize {
         visited_small_caves,
     }) = path_stack.pop()
     {
-        if current_cave == "end" {
+        if current_cave == END_CAVE_LABEL {
             count += 1;
         } else {
-            let candidates = input.get(current_cave).unwrap().iter().filter_map(|&cave| {
-                if is_big_cave(cave) {
+            let candidates = map.get(&current_cave).unwrap().iter().filter_map(|&cave| {
+                if big_cave_ids.contains(&cave) {
                     Some(P1State {
                         current_cave: cave,
                         visited_small_caves: visited_small_caves.clone(),
                     })
-                } else if !visited_small_caves.contains(cave) {
+                } else if !visited_small_caves.contains(&cave) {
                     let mut new_visited_small_caves = visited_small_caves.clone();
                     new_visited_small_caves.insert(cave);
                     Some(P1State {
@@ -82,18 +104,21 @@ fn p1(input: &Input) -> usize {
     count
 }
 
-struct P2State<'a> {
-    current_cave: &'a str,
-    visited_small_caves: HashSet<&'a str>,
+struct P2State {
+    current_cave: u8,
+    visited_small_caves: HashSet<u8>,
     has_visited_small_cave_twice: bool,
 }
 
 fn p2(input: &Input) -> usize {
     let initial_state = P2State {
-        current_cave: "start",
-        visited_small_caves: ["start"].into_iter().collect::<HashSet<_>>(),
+        current_cave: START_CAVE_LABEL,
+        visited_small_caves: HashSet::from([START_CAVE_LABEL]),
         has_visited_small_cave_twice: false,
     };
+    let Input {
+        map, big_cave_ids, ..
+    } = input;
     let mut path_stack = vec![initial_state];
     let mut count = 0;
     while let Some(P2State {
@@ -102,19 +127,19 @@ fn p2(input: &Input) -> usize {
         has_visited_small_cave_twice,
     }) = path_stack.pop()
     {
-        if current_cave == "end" {
+        if current_cave == END_CAVE_LABEL {
             count += 1;
         } else {
-            let candidates = input.get(current_cave).unwrap().iter().filter_map(|&cave| {
-                if is_big_cave(cave) {
+            let candidates = map.get(&current_cave).unwrap().iter().filter_map(|&cave| {
+                if big_cave_ids.contains(&cave) {
                     Some(P2State {
                         current_cave: cave,
                         visited_small_caves: visited_small_caves.clone(),
                         has_visited_small_cave_twice,
                     })
-                } else if cave == "start" {
+                } else if cave == START_CAVE_LABEL {
                     None
-                } else if !visited_small_caves.contains(cave) {
+                } else if !visited_small_caves.contains(&cave) {
                     let mut new_visited_small_caves = visited_small_caves.clone();
                     new_visited_small_caves.insert(cave);
                     Some(P2State {
