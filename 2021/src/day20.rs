66bed0671330f11f6c58f062632b0bbe5978779e
Huáng Jùnliàng <jlhwung@gmail.com>
2021-12-20 22:01:06 -0500

    perf: replace enhance_twice
    2x faster

--- 2021/src/day20.rs
+++ 2021/src/day20.rs
@@ -19,6 +19,10 @@ type Point = [Number; 2];
 
 type Algorithm = Vec<bool>;
 
+// Delta array ordererd as
+// 8 7 6
+// 5 4 3
+// 2 1 0
 const DELTA: [Point; 9] = [
     [1, 1],
     [0, 1],
@@ -31,49 +35,11 @@ const DELTA: [Point; 9] = [
     [-1, -1],
 ];
 
-const PADDING: i16 = 2;
+const PADDING: i16 = 1;
+const ALGORITHM_SIZE: usize = 512;
 
-fn add(lhs: &Point, rhs: &Point) -> Point {
-    [lhs[0] + rhs[0], lhs[1] + rhs[1]]
-}
-
-fn enhance_at_is_light(
-    point: &Point,
-    light_pixels: &HashSet<Point>,
-    algorithm: &[bool],
-) -> bool {
-    let mut index = 0;
-    for (i, delta) in DELTA.iter().enumerate() {
-        let neighbor = add(point, delta);
-        if light_pixels.contains(&neighbor) {
-            index |= 1 << i
-        }
-    }
-    algorithm[index]
-}
-
-fn enhance_twice_at_is_light(
-    point: &Point,
-    light_pixels: &HashSet<Point>,
-    algorithm: &[bool],
-) -> bool {
-    let mut index = 0;
-    for (i, delta) in DELTA.iter().enumerate() {
-        if enhance_at_is_light(&add(point, delta), light_pixels, algorithm) {
-            index |= 1 << i
-        }
-    }
-    algorithm[index]
-}
-
-#[derive(Clone, Debug)]
-struct Image {
-    x_min: Number,
-    x_max: Number,
-    y_min: Number,
-    y_max: Number,
-    light_pixels: HashSet<Point>,
-}
+// An image is a HashSet of lit pixels
+type Image = HashSet<Point>;
 
 struct Input {
     algorithm: Algorithm,
@@ -90,75 +56,89 @@ fn process(raw: &str) -> Input {
             _ => None,
         })
         .collect::<Vec<_>>();
-    assert_eq!(algorithm.len(), 512);
+    assert_eq!(algorithm.len(), ALGORITHM_SIZE);
 
-    let mut light_pixels = HashSet::new();
-    let (mut x_max, mut y_max) = (0, -1);
+    let mut image = HashSet::new();
     for (j, line) in image_rep.lines().enumerate() {
-        x_max = line.len() - 1;
         for (i, ch) in line.chars().enumerate() {
             if ch == '#' {
-                light_pixels.insert([i as Number, j as Number]);
+                image.insert([i as Number, j as Number]);
             }
         }
-        y_max += 1;
     }
 
-    Input {
-        algorithm,
-        image: Image {
-            x_min: 0,
-            x_max: x_max as Number,
-            y_min: 0,
-            y_max,
-            light_pixels,
-        },
-    }
+    Input { algorithm, image }
 }
 
-fn enhance_twice(image: &Image, algorithm: &[bool]) -> Image {
-    let Image {
-        mut x_min,
-        mut x_max,
-        mut y_min,
-        mut y_max,
-        light_pixels: orig_light_pixels,
-    } = image;
-    x_min -= PADDING;
-    x_max += PADDING;
-    y_min -= PADDING;
-    y_max += PADDING;
+fn enhanced_pixel_is_light(
+    [x0, y0]: &Point,
+    image: &Image,
+    algorithm: &[bool],
+    [x_min, y_min]: &Point,
+    [x_max, y_max]: &Point,
+    border_is_light: bool,
+) -> bool {
+    let mut index = 0;
+    for (i, [dx, dy]) in DELTA.iter().enumerate() {
+        let neighbor = [x0 + dx, y0 + dy];
+        let [x, y] = &neighbor;
+        if (border_is_light && (x < x_min || x > x_max || y < y_min || y > y_max))
+            || image.contains(&neighbor)
+        {
+            index |= 1 << i
+        }
+    }
+    algorithm[index]
+}
 
-    let mut light_pixels = HashSet::new();
-    for x in x_min..=x_max {
-        for y in y_min..=y_max {
-            if enhance_twice_at_is_light(&[x, y], orig_light_pixels, algorithm) {
-                light_pixels.insert([x, y]);
+fn enhance(image: &Image, algorithm: &[bool], border_is_light: &mut bool) -> Image {
+    let (mut x_min, mut y_min, mut x_max, mut y_max) =
+        (Number::MAX, Number::MAX, Number::MIN, Number::MIN);
+
+    for p in image.iter() {
+        x_min = Number::min(x_min, p[0]);
+        y_min = Number::min(y_min, p[1]);
+        x_max = Number::max(x_max, p[0]);
+        y_max = Number::max(y_max, p[1]);
+    }
+
+    let mut output = Image::new();
+    for x in x_min - PADDING..=x_max + PADDING {
+        for y in y_min - PADDING..=y_max + PADDING {
+            if enhanced_pixel_is_light(
+                &[x, y],
+                image,
+                algorithm,
+                &[x_min, y_min],
+                &[x_max, y_max],
+                *border_is_light,
+            ) {
+                output.insert([x, y]);
             }
         }
     }
-    Image {
-        x_min,
-        x_max,
-        y_min,
-        y_max,
-        light_pixels,
+
+    // flip border pixels accoriding to algorithms
+    *border_is_light =
+        (algorithm[0] && !*border_is_light) || (algorithm[ALGORITHM_SIZE - 1] && *border_is_light);
+    output
+}
+
+fn count_enhanced_light_pixels(input: &Input, round: usize) -> usize {
+    let mut border_is_light = false;
+    let mut enhanced = input.image.clone();
+    for _ in 0..round {
+        enhanced = enhance(&enhanced, &input.algorithm, &mut border_is_light);
     }
+    enhanced.len()
 }
 
 fn p1(input: &Input) -> usize {
-    let enhanced = enhance_twice(&input.image, &input.algorithm);
-    enhanced.light_pixels.len()
+    count_enhanced_light_pixels(input, 2)
 }
 
-const TIME: usize = 50;
-
 fn p2(input: &Input) -> usize {
-    let mut enhanced = input.image.clone();
-    for _ in 0..TIME / 2 {
-        enhanced = enhance_twice(&enhanced, &input.algorithm);
-    }
-    enhanced.light_pixels.len()
+    count_enhanced_light_pixels(input, 50)
 }
 
 #[cfg(test)]
